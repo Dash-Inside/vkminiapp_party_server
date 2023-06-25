f8720051a3559e2570d73c4e38c993f74dcb62b0
pocket-red-fox <pocket.red.fox@gmail.com>
2023-06-25 17:20:37 +0300

    refactor: change project structure
    Co-authored-by: @danwirele

---
+++ internal/cmd/server.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"context"
+	"partyserver/internal/logger"
+	"partyserver/internal/router"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"go.uber.org/fx"
+)
+
+func loop(lifecycle fx.Lifecycle, router *gin.Engine) {
+	lifecycle.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			address := viper.GetString(`ADDRESS`)
+			port := viper.GetString(`PORT`)
+
+			if err := router.Run(address + ":" + port); err != nil {
+				return err
+			}
+
+			return nil
+		},
+		OnStop: func(context.Context) error {
+			return nil
+		},
+	})
+}
+
+func RunServer() error {
+	app := fx.New(
+		fx.Provide(logger.NewLogger),
+		fx.Provide(router.NewRouterDelivery),
+		fx.Invoke(loop),
+	)
+
+	if err := app.Err(); err != nil {
+		return err
+	}
+
+	app.Run()
+
+	return nil
+}
